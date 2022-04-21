4f19b4c251662c89752a03e1682afc8737feccbb
mohitleo03 <mohitsmalik2038@gmail.com>
2022-04-21 15:39:56 +0530

    update

--- PythonDjangoApp/myapp/app/views.py
+++ PythonDjangoApp/myapp/app/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+# def index (request):
+#     return HttpResponse("<h1>Hello from Django...</h1>")
+
+# def index (request):
+#     name = 'Mohit'
+#     return render(request, 'index.html',context={'name' : name})
+
 def index (request):
-    return HttpResponse("<h1>Hello from Django...</h1>")
+    name = 'Mohit'
+    return render(request, 'login.html')
+
+def login(request):
+    username = request.GET['u_name']
+    password = request.GET['u_password']
+    if username == 'mohit' and password == 'mohit03':
+        msg = 'Login successful'
+    else:
+        msg = "Login Failed"
+    return render(request,'index.html',context={'msg':msg, 'name':username})
